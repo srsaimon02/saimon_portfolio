c95f13b0d366eecc9f12ef74df79c3fb2ef4fd92
Saimon <srs.saimon02@gmail.com>
2026-07-19 16:44:06 +0100

    lots of updates and improvements(i forgot to commit for a while) oh and added a little quiz app

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const myName = "Saimon";
-let myCourse = "Computer science with Masters";
-const myUni = "University of Nottingham";
-
-const heading = document.querySelector("h1");
-
-heading.addEventListener("click", function() {
-    heading.style.color = "orange";
-    heading.textContent = "You clicked me!!"
-});
-
-
-
-function introduce(name, course, university){
-    console.log(`Hello, my name is ${name} and I study ${course} at ${university}!`)
-}
-
-myCourse = "nothing";
 
 function randomColour(){
     const char = "0123456789ABCDEF";
@@ -30,10 +12,11 @@ return colour;
 
 function generatePalette(){
     const palette = document.querySelector("#palette");
+    const PALETTE_SIZE = 5;
 
     palette.innerHTML = "";
 
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < PALETTE_SIZEs; i++) {
         const colour = randomColour();
 
         const swatch = document.createElement("button");
@@ -56,7 +39,84 @@ function generatePalette(){
 }
 
 
+
 const generateBtn = document.querySelector("#generate-btn");
 generateBtn.addEventListener("click", generatePalette);
 
-introduce(myName, myCourse, myUni);
+
+const quizQuestions = [
+    {
+        text : "What does HTML stand for?",
+        options : ["Hyper Text Markup Language", "High Tech Modern Look", "Hello To My Lamb"],
+        correctAnswer : 0
+    },
+    {
+        text : "What does CSS stand for?",
+        options : ["Cool Style Sheets", "Cascading Style Sheets", "Cray Shee Shee"],
+        correctAnswer : 1
+    },
+    {
+        text : "What does JS stand for?",
+        options : ["Jail style", "Jong Song", "JavaScript"],
+        correctAnswer : 2
+    }
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const quizQuestionEl = document.querySelector("#quiz-question");
+const quizProgressEl = document.querySelector("#quiz-progress");
+const quizScoreEl = document.querySelector("#quiz-score");
+const quizOptionsEl = document.querySelector("#quiz-options");
+
+function showQuestion() {
+    const question = quizQuestions[currentQuestionIndex];
+
+    quizQuestionEl.textContent = question.text;
+    quizProgressEl.textContent = `Question ${currentQuestionIndex + 1} of ${quizQuestions.length}`;
+    quizScoreEl.textContent = `Score: ${score}`;
+
+    quizOptionsEl.innerHTML = "";
+    
+    for (let i = 0; i < question.options.length; i++) {
+        const optBtn = document.createElement("button");
+        optBtn.textContent = question.options[i];
+
+        optBtn.addEventListener("click", function(){
+            handleAnswer(i);
+        });
+
+
+        quizOptionsEl.appendChild(optBtn)
+    }
+}
+
+function handleAnswer(selectedIndex){
+
+    const question = quizQuestions[currentQuestionIndex];
+    if (selectedIndex === question.correctAnswer) {
+        score++;
+    }
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < quizQuestions.length) {
+        showQuestion();
+    }
+    else {
+        showFinalScore();
+    }
+
+}
+
+    function showFinalScore() {
+        quizProgressEl.textContent = "Quiz Completed!";
+        quizOptionsEl.innerHTML = "";
+        quizScoreEl.textContent = `Final score: ${score} out of ${quizQuestions.length}`;
+        /* add reset quiz + shuffle at restart */
+
+    }
+
+showQuestion();
+
